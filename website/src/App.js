46f85c9a365d0b7a9f231eba46bc89565de2ed5a
JulianReid541 <reidjulian@outlook.com>
2020-12-21 13:42:18 -0800

    header and about us page completed

--- website/src/App.js
+++ website/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+//Pages
 import PageWrapper from './components/PageWrapper';
 import Home from './components/Pages/Home';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import About from './components/Pages/About';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <PageWrapper>
-            <Route path="/" component={Home}/>
+            <Route exact={true} path="/" component={Home}/>
+            <Route path="/about" component={About} />
         </PageWrapper>
       </Router>
     );
