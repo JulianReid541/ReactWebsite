e53ca6b2be662c1708eef83fa246742f737e906b
JulianReid541 <reidjulian@outlook.com>
2020-12-22 10:31:08 -0800

    routing completed

--- website/src/App.js
+++ website/src/App.js
@@ -6,6 +6,9 @@ import PageWrapper from './components/PageWrapper';
 import Home from './components/Pages/Home';
 import About from './components/Pages/About';
 import Contact from './components/Pages/Contact';
+import Services from './components/Common/Services';
+import Portfolio from './components/Common/Portfolio';
+import Team from './components/Common/Team';
 
 class App extends Component {
   render() {
@@ -13,6 +16,9 @@ class App extends Component {
       <Router>
         <PageWrapper>
             <Route exact={true} path="/" component={Home}/>
+            <Route path="/services" component={Services} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/team" component={Team} />
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
         </PageWrapper>
