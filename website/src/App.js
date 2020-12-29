f3e715e89112343061d13c67d9be7ae2cb0363bd
JulianReid541 <reidjulian@outlook.com>
2020-12-29 14:04:29 -0800

    started work on the admin side. initial setup before redux

--- website/src/App.js
+++ website/src/App.js
@@ -9,19 +9,49 @@ import Contact from './components/Pages/Contact';
 import Services from './components/Common/Services';
 import Portfolio from './components/Common/Portfolio';
 import Team from './components/Common/Team';
+import AdminWrapper from './components/AdminWrapper';
+import Login from './components/Pages/Login';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <PageWrapper>
-            <Route exact={true} path="/" component={Home}/>
+
+        <Route
+          path="/admin"
+          render={props => (
+            <AdminWrapper>
+              <Login />
+            </AdminWrapper>
+          )}
+        />
+            <Route exact={true} path="/" 
+              render={props => (
+                <PageWrapper>
+                  <Home {...props} />
+                </PageWrapper>
+
+              )}
+            />
             <Route path="/services" component={Services} />
             <Route path="/portfolio" component={Portfolio} />
             <Route path="/team" component={Team} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-        </PageWrapper>
+            <Route path="/about" 
+              render={props => (
+                <PageWrapper>
+                  <About {...props} />
+                </PageWrapper>
+
+              )}
+            />
+            <Route path="/contact" 
+              render={props => (
+                <PageWrapper>
+                  <Contact {...props} />
+                </PageWrapper>
+
+              )}
+            />
       </Router>
     );
   } 
